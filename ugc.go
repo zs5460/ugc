e18e090c7ba340ae78a865bf24969ff5632f14fa
zs5460 <zs5460@gmail.com>
2019-02-25 17:17:25 +0800

    feat: add GTU UTG

---
+++ ugc.go
@@ -0,0 +1,28 @@
+package ugc
+
+import (
+	"bytes"
+	"golang.org/x/text/encoding/simplifiedchinese"
+	"golang.org/x/text/transform"
+	"io/ioutil"
+)
+
+// GTU ...
+func GTU(s []byte) ([]byte, error) {
+	reader := transform.NewReader(bytes.NewReader(s), simplifiedchinese.GBK.NewDecoder())
+	d, e := ioutil.ReadAll(reader)
+	if e != nil {
+		return nil, e
+	}
+	return d, nil
+}
+
+// UTG ...
+func UTG(s []byte) ([]byte, error) {
+	reader := transform.NewReader(bytes.NewReader(s), simplifiedchinese.GBK.NewEncoder())
+	d, e := ioutil.ReadAll(reader)
+	if e != nil {
+		return nil, e
+	}
+	return d, nil
+}
